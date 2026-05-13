922dbf184db596b180141bf54cbdbe030393e9fd
Gustavo Pozzebon <guguplorenz@gmail.com>
2026-05-13 20:56:21 -0300

    Working
    The website now draw the circle and counts how many pixels are needed

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function BuildGrid(){
+    console.log("")
     const Width = parseInt(document.getElementById("Width").value)
-    const GridWidth = Width+2
-    const CellWidth = 100/GridWidth
+    var GridWidth = Width+2
+
+    if (GridWidth%2 == 0){
+        GridWidth+=1
+    }
 
     var Grid = document.getElementById("Grid")
     var Text = ""
@@ -12,6 +16,52 @@ function BuildGrid(){
 
     Grid.innerHTML = Text+"<div id='Center'></div>"
     Grid.style.gridTemplateColumns = "repeat("+GridWidth+",1fr)"
+
+    DrawCircle()
+}
+
+function DrawCircle(){
+    const Width = parseInt(document.getElementById("Width").value)
+    var GridCellsWidth = Width+2
+
+    if (GridCellsWidth%2 == 0){
+        GridCellsWidth+=1
+    }
+
+    const CellWidth = (100/GridCellsWidth)
+    
+    
+    const Piece = (2*Math.PI)/(GridCellsWidth*20)
+    const Circle = document.getElementById("Circle")
+    const Pixels = document.getElementById("Pixels")
+
+    var Positions = []
+
+    Circle.innerHTML = ""
+
+    for(var i = 0; i<=Width*40; i++){
+        var Radians = i*Piece
+
+        const X = Math.round(Math.cos(Radians)*Math.trunc(Width/2))
+        const Y = Math.round(Math.sin(Radians)*Math.trunc(Width/2))
+
+        const PaintedCell = document.createElement("div")
+
+        PaintedCell.classList.add("PaintedCell")
+
+        PaintedCell.style.left = ((GridCellsWidth / 2 + X) * CellWidth) + "%"
+        PaintedCell.style.top  = ((GridCellsWidth / 2 + Y) * CellWidth) + "%"
+        PaintedCell.style.width = CellWidth+"%"
+        PaintedCell.style.height = CellWidth+"%"
+
+        Circle.appendChild(PaintedCell)
+
+        if(!Positions.some(pos => pos.every((val, i) => val === [X,Y][i]))) {
+            Positions.push([X,Y]);
+        }
+    }
+    Pixels.innerText = "Pixels: "+Positions.length
+
 }
 
 BuildGrid()
